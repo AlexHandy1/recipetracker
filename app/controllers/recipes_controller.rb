8696ea77234d21f00d698629a3800912c1b8330c
Alex Handy <handy437@gmail.com>
2015-08-15 16:06:45 +0100

    Pagination added for 10 recipes per page - list recipes user story completed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.paginate(:per_page => 10, :page => params[:page])
   end
 
   def show
--- spec/features/recipe_list_spec.rb
+++ spec/features/recipe_list_spec.rb
@@ -54,4 +54,52 @@ feature 'RecipeList' do
     click_link 'Lemon Chicken'
     expect(current_path).to eq ('/recipes/1')
   end
+
+  #assume random ordering
+  scenario 'When there are more than 10 recipes, only the the first 10 recipes are shown' do
+    #set-up recipes to fill 1 page
+    10.times do |x|
+      @recipe = Recipe.create(name: "Lemon Chicken", cooking_time: "30 minutes")
+      @recipe.ingredients.create(name: "Chicken")
+      @recipe.ingredients.create(name: "Lemon")
+      @recipe.ingredients.create(name: "Thyme")
+    end
+
+     #create recipe that won't be showing on first page - would sit on second page
+    @recipe = Recipe.create(name: "Beef Stroganoff", cooking_time: "30 minutes")
+    @recipe.ingredients.create(name: "Beef")
+    @recipe.ingredients.create(name: "Mustard")
+    @recipe.ingredients.create(name: "Mushrooms")
+
+    visit '/'
+    expect(page).not_to have_content("Beef Stroganoff")
+  end
+
+  scenario 'When there are more than 20 recipes and click on second page (following pagination navigation markers) user can see next 10 recipes' do
+    #set-up recipes to fill 2 pages bar 1 target recipe
+    19.times do |x|
+      @recipe = Recipe.create(name: "Lemon Chicken", cooking_time: "30 minutes")
+      @recipe.ingredients.create(name: "Chicken")
+      @recipe.ingredients.create(name: "Lemon")
+      @recipe.ingredients.create(name: "Thyme")
+    end
+
+    #create target recipe on second page
+    @recipe = Recipe.create(name: "Beef Stroganoff", cooking_time: "30 minutes")
+    @recipe.ingredients.create(name: "Beef")
+    @recipe.ingredients.create(name: "Mustard")
+    @recipe.ingredients.create(name: "Mushrooms")
+
+    #create recipe that won't be showing on second page - would sit on third page
+    @recipe = Recipe.create(name: "Caesar Salad", cooking_time: "25 minutes")
+    @recipe.ingredients.create(name: "Lettuce")
+    @recipe.ingredients.create(name: "Croutons")
+    @recipe.ingredients.create(name: "Parmesan")
+    visit '/'
+    click_link '2'
+    expect(page).to have_content("Beef Stroganoff")
+    expect(page).not_to have_content("Caesar Salad")
+    click_link '3'
+    expect(page).to have_content("Caesar Salad")
+  end
 end
